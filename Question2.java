fa974efb5566b231869a7f1a32a6a32102c2828d
griffinkoh <s10223376@connect.np.edu.sg>
2022-04-19 16:19:50 +0800

    Question 2

--- Question2.java
+++ Question2.java
@@ -10,14 +10,9 @@ public class Question2
      * Hint: in.nextDouble() to read double
      */
      
-    Scanner obj1= new Scanner(System.in);
-    System.out.println("Enter your height: ");
-    double height = obj1.nextDouble();
-
-    
-    Scanner obj2= new Scanner(System.in);
-    System.out.println("Enter your height: ");
-    double weight = obj2.nextDouble();
+    Scanner in= new Scanner(System.in);
+    double height = in.nextDouble();
+    double weight = in.nextDouble();
 
     double BMI = weight / Math.pow(height, 2);
 
